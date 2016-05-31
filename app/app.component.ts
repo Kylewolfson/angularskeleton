132d0c1bb767a8cdc886cc53570773475082ed52
Andrew Russo and Kyle Wolfson <andrew@>
2016-05-31 11:57:39 -0700

    Angular2 skeleton used for practicing Angular2 programming.

---
+++ app/app.component.ts
@@ -0,0 +1,13 @@
+@Component({
+  selector: 'my-app',
+  template:`
+    <div class="container">
+      <h1>Skeleton Angular2 App!</h1>
+    </div>
+      `
+})
+
+export class AppComponent {
+
+}
+import { Component } from 'angular2/core';
